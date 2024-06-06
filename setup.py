5142addc2f08e6bd99091f4405e62c8fdd5cdfef
Kungl <akos-ferenc.kungl@zeiss.com>
2024-06-06 12:30:45 +0200

    Add conda env and setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup, find_packages
+
+ci_version = os.environ.get('BUILD_BUILDNUMBER', '0.0.1')
+
+
+setup(
+    name='python_example_package',
+    version=ci_version,
+    packages=find_packages() + [
+        "pep"
+    ],
+    package_dir={"pep": "src/pep"},
+    author='Akos F Kungl',
+    description='Example Package to start other packages from',
+    url='https://github.com/afkungl/python_example_package',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=[
+        'numpy==1.26.4',
+        'pytest'
+    ]
+)
---
+++ tests/test_load_project.py
@@ -0,0 +1,3 @@
+def test_loading_project():
+
+    import pep
