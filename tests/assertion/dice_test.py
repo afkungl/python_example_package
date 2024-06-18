1e39232d22003b1b85ce2241b20c218690edc77c
Kungl <akos-ferenc.kungl@zeiss.com>
2024-06-18 10:59:56 +0200

    Improve code quality in tests

--- tests/assertion/dice_test.py
+++ tests/assertion/dice_test.py
@@ -1,3 +1,12 @@
+"""
+This module provides a DiceSimulator class for simulating the rolling of a dice.
+
+Usage:
+    a_dice = DiceSimulator(6)
+    a_dice.throw()
+
+"""
+
 import pytest
 from pep import dice
 
@@ -6,9 +15,21 @@ thow_dice_params = [(4, [1, 2, 3, 4]),
                     (20, list(range(1, 21)))]
 
 
-@pytest.mark.assertion
-@pytest.mark.parametrize('sides, results', thow_dice_params)
-def test_throw_dice(sides, results):
+def test_throw_dice(sides: int, results: list) -> None:
+    """
+    Test function for the DiceSimulator class in the dice module.
+
+    Args:
+        sides: An integer representing the number of sides on the dice.
+        results: A list of possible results from rolling the dice.
+
+    Returns:
+        None.
+    """
     a_dice = dice.DiceSimulator(sides)
     for _ in range(20):
-        assert (a_dice.throw() in results)
+        assert a_dice.throw() in results
+
+
+if __name__ == '__main__':
+    pytest.main()
