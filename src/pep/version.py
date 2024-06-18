15faea9c2c518a91509d9a60ba35f09f1cf3d250
Akos Kungl <afkungl@users.noreply.github.com>
2024-06-18 11:03:56 +0200

    Feature/improve code quality (#16)
    * Improve style
    
    * Improve code quality
    
    * Add version to source code
    
    * Improve code quality in tests
    
    * Improve code quality
    
    ---------
    
    Co-authored-by: Kungl <akos-ferenc.kungl@zeiss.com>

---
+++ src/pep/version.py
@@ -0,0 +1,4 @@
+"""
+Deliver version with sourcecode
+"""
+__version__ = '0.0.1'
--- tests/examples/run_example_game.py
+++ tests/examples/run_example_game.py
@@ -1,17 +1,31 @@
+"""
+This module runs a game of JustLine using a dice simulator.
+
+Dependencies:
+- pep.dice
+- pep.games.line
+"""
+
 from pep import dice
 from pep.games import line
 
 
-def run_a_game():
-    # get a dice
-    a_dice = dice.DiceSimulator(6)
+def run_a_game() -> None:
+    """
+    Runs a game of JustLine using a dice simulator.
 
-    # get a board
-    a_board = line.JustLine(20)
+    Returns:
+        None.
+    """
+    # Get a dice
+    a_dice: dice.DiceSimulator = dice.DiceSimulator(6)
 
-    max_throws = 10
+    # Get a board
+    a_board: line.JustLine = line.JustLine(20)
+
+    max_throws: int = 10
     for _ in range(max_throws):
-        throw_res = a_dice.throw()
+        throw_res: int = a_dice.throw()
         a_board.step(throw_res)
 
         if a_board.state_finished:
