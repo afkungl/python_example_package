65435fac65a726bdac5a2852a8fd29a80c059bbc
Kungl <akos-ferenc.kungl@zeiss.com>
2024-06-20 10:57:26 +0200

    Fix test

--- tests/assertion/dice_test.py
+++ tests/assertion/dice_test.py
@@ -14,6 +14,7 @@ thow_dice_params = [(4, [1, 2, 3, 4]),
                     (6, [1, 2, 3, 4, 5, 6]),
                     (20, list(range(1, 21)))]
 
+
 @pytest.mark.assertion
 @pytest.mark.parametrize('sides, results', thow_dice_params)
 def test_throw_dice(sides: int, results: list) -> None:
